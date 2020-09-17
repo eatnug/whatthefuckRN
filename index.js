56ba0fc0775ec70ca8b4407d7362b948168e9b63
Eatnug <wlq4568@gmail.com>
2020-09-17 22:05:01 +0900

    feat(permission): react-native-permissions 세팅 & sentry 이니셜라이징 수정

--- index.js
+++ index.js
@@ -5,5 +5,12 @@ import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
 import App from '@/App';
 import { name as appName } from './app.json';
+import { SENTRY_DSN } from '@/constants';
+import * as Sentry from '@sentry/react-native';
+
+!__DEV__ &&
+  Sentry.init({
+    dsn: SENTRY_DSN
+  });
 
 AppRegistry.registerComponent(appName, () => App);
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import RootStackNavigator from '@/naviagtions';
 import styled from 'styled-components/native';
-import * as Sentry from '@sentry/react-native';
-import { SENTRY_DSN } from '@/constants';
+// import * as Sentry from '@sentry/react-native';
+// import { SENTRY_DSN } from '@/constants';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { configureStore } from '@/redux';
@@ -16,25 +16,17 @@ const codePushOptions = {
 
 const { store, persistor } = configureStore();
 
-const App = () => {
-  useEffect(() => {
-    Sentry.init({
-      dsn: SENTRY_DSN
-    });
-  }, []);
-
-  return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <NavigationContainer>
-          <StyledSafeAreaView>
-            <RootStackNavigator />
-          </StyledSafeAreaView>
-        </NavigationContainer>
-      </PersistGate>
-    </Provider>
-  );
-};
+const App = () => (
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <NavigationContainer>
+        <StyledSafeAreaView>
+          <RootStackNavigator />
+        </StyledSafeAreaView>
+      </NavigationContainer>
+    </PersistGate>
+  </Provider>
+);
 
 const StyledSafeAreaView = styled(SafeAreaView)`
   flex: 1;
